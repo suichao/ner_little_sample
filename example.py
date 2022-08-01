848a993c693b8d805801840a9f2dcb5e8293d2fd
notebook <1342500100@qq.com>
2022-08-02 00:32:39 +0800

    同步数据集

--- example.py
+++ example.py
@@ -5,9 +5,8 @@ from tokenizer_utils import FSNERTokenizerUtils
 from utils import pretty_embed
 
 query_texts = [
-    "Does Luke's serve lunch?",
-    "Chang does not speak Taiwanese very well.",
-    "I like Berlin."
+    "万通地产设计总监刘克峰；",
+    "我最喜欢的小说是西游记"
 ]
 
 # Each list in supports are the examples of one entity type
@@ -15,31 +14,36 @@ query_texts = [
 # Each sentence should have only one pair of [E] ... [/E]
 
 support_texts = {
-    "Restaurant": [
-        "What time does [E] Subway [/E] open for breakfast?",
-        "Is there a [E] China Garden [/E] restaurant in newark?",
-        "Does [E] Le Cirque [/E] have valet parking?",
-        "Is there a [E] McDonalds [/E] on main street?",
-        "Does [E] Mike's Diner [/E] offer huge portions and outdoor dining?"
-    ],
-    "Language": [
-        "Although I understood no [E] French [/E] in those days , I was prepared to spend the whole day with Chien - chien .",
-        "like what the hell 's that called in [E] English [/E] ? I have to register to be here like since I 'm a foreigner .",
-        "So , I 'm also working on an [E] English [/E] degree because that 's my real interest .",
-        "Al - Jazeera TV station , established in November 1996 in Qatar , is an [E] Arabic - language [/E] news TV station broadcasting global news and reports nonstop around the clock .",
-        "They think it 's far better for their children to be here improving their [E] English [/E] than sitting at home in front of a TV . \"",
-        "The only solution seemed to be to have her learn [E] French [/E] .",
-        "I have to read sixty pages of [E] Russian [/E] today ."
-    ]
+        "place": [
+            "[E]广州[/E]即将举行亚运会",
+            "还记得[E]北京[/E]奥运会的开幕式吗",
+            "知道[E]上海[/E]在哪里吗",
+            "[E]佛山[/E]是著名的武术之乡",
+            "[E]湛江[/E]是个美丽的海滨城市"
+          ],
+        "person": [
+        "浙商银行企业信贷部[E]叶老桂[/E]博士则从另一个角度对五道门槛进行了解读。",
+        "万通地产设计总监[E]刘克峰[/E]；",
+        "方传柳实习生[E]王梦菲[/E]",
+        "乐成中心的ceo[E]周东权[/E]先生",
+        "接受锦旗的刑警[E]李济舟[/E]还原了案件经过。"
+      ],
+       "book": [
+        "【吴海花】：我代表北京《[E]百姓地产[/E]》杂志感谢大家赶来参加今天《新老金融街争锋—",
+        "沃霍尔手绘杰作“[E]大坎贝尔浓汤罐头[/E]”（蔬菜）是波普艺术的代表作",
+        "花5000钱购买一本《[E]华胥梦天记[/E]》。",
+        "银监会日前印发的《[E]关于进一步规范信用卡业务的通知[/E]》规定，",
+        "中广网北京6月24日消息（记者张晶）据中国之声《[E]新闻纵横[/E]》7时42分报道，"
+      ],
 }
 
 device = 'cpu'
 
-tokenizer = FSNERTokenizerUtils("pretrain/fsner/")
+tokenizer = FSNERTokenizerUtils("checkpoints/model/")
 queries = tokenizer.tokenize(query_texts).to(device)
 supports = tokenizer.tokenize(list(support_texts.values())).to(device)
 
-model = FSNERModel("pretrain/fsner/")
+model = FSNERModel("checkpoints/model/")
 model.to(device)
 
 p_starts, p_ends = model.predict(queries, supports)
